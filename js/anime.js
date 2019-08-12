d7724b1fc1bec593682c358b1a69c7dd376477a9
xinyi <xinyi2017mm@gmail.com>
2019-08-11 23:38:21 -0700

    add

--- js/anime.js
+++ js/anime.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
     var box = document.getElementsByClassName('typing-text')[0];
     var str = box.innerHTML;
     var i = 0;
-    box.innerHTML = "I am a front-end developer.I can build a website with \
-    desiralbe view and responsive features.\
-    I am good at bootstrap,responsive design and html, css, sass, \
-    jQuery(including its plungin), front-end ";
+    box.innerHTML = "I am a full stak developer with 3 years of experiene. \
+    I can connect web apps to databases, manage data resources with modern framework including Express.js \
+    The databases I am experienced with include MySQL, MongoDB, and Firebase. I can also build responsive \
+    web apps with html, css, javasript and various libraries \
+    including jQuery and bootstrap. I am experiened with \
+    deploying applications to ";
     var typing = setInterval(function() {
         box.innerHTML += str[i++];
         i >= str.length && clearInterval(typing);
